805c619ee11c2d8713dcbc308e7a55c86a3db970
luth9r <dzadan73@gmail.com>
2026-08-28 19:53:11 +0200

    fix: remove xdg-desktop-portal-gtk, use KDE portal as default file picker

--- hosts/common/hyprland.nix
+++ hosts/common/hyprland.nix
@@ -25,21 +25,24 @@
   # GVFS for virtual filesystems, trash, network shares, and file-open handlers
   services.gvfs.enable = true;
 
-  # Enable XDG Desktop Portals for Wayland screen sharing and KDE/Qt file pickers
+  # Enable XDG Desktop Portals: Hyprland for screen capture, KDE for everything else
   xdg.portal = {
     enable = true;
     extraPortals = [
       pkgs.xdg-desktop-portal-hyprland
       pkgs.kdePackages.xdg-desktop-portal-kde
-      pkgs.xdg-desktop-portal-gtk
     ];
     config = {
       common = {
-        default = [ "hyprland" "kde" "gtk" ];
+        default = [ "kde" ];
+        "org.freedesktop.impl.portal.ScreenCast" = [ "hyprland" ];
+        "org.freedesktop.impl.portal.Screenshot" = [ "hyprland" ];
         "org.freedesktop.impl.portal.FileChooser" = [ "kde" ];
       };
       hyprland = {
-        default = [ "hyprland" "kde" "gtk" ];
+        default = [ "kde" ];
+        "org.freedesktop.impl.portal.ScreenCast" = [ "hyprland" ];
+        "org.freedesktop.impl.portal.Screenshot" = [ "hyprland" ];
         "org.freedesktop.impl.portal.FileChooser" = [ "kde" ];
       };
     };
