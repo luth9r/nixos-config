b5b454a4eb509a2ea54385f3671c97dd70d0055a
luth9r <dzadan73@gmail.com>
2026-08-30 01:00:27 +0200

    feat(boot): limit systemd-boot menu entries to maximum 5 generations

--- hosts/common/core.nix
+++ hosts/common/core.nix
@@ -3,6 +3,7 @@
 {
   # Bootloader settings
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Networking
